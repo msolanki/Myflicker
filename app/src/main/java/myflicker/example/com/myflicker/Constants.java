2d32784b34a3cd0fccb42bed94d1374963f16191
msolanki <solankimonali31@gmail.com>
2015-05-08 21:17:48 -0700

    Myflicker
    myflicker

---
+++ app/src/main/java/myflicker/example/com/myflicker/Constants.java
@@ -0,0 +1,11 @@
+package myflicker.example.com.myflicker;
+
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "dc7e8a6a2087e7e788a56d5a72d129bf";
+    public static final String SECRET = "752a8d74c1bbc8af";
+    public static final String RADIUS = "1";
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+}
